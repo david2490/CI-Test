2bbccdd5909a794c0a2eff95e022e06f5ebeb05e
david.abrego <david.abrego@onelinkbpo.com>
2018-05-05 14:07:33 -0600

    primer commit

---
+++ CI-UNIT-LAB/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CI-UNIT-LAB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CI-UNIT-LAB")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef005398-f915-4edc-89d4-636554172e02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CI-UNIT-LAB/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI_UNIT_LAB
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void sumar()
+        {
+            int a = 10;
+            int b = 10;
+            int result = 10;
+            CI_TEST.Controllers.HomeController controller = new CI_TEST.Controllers.HomeController();
+            Assert.AreEqual(result, controller.sumar(a, b));
+        }
+    }
+}
